ee7e807eb539cd8f0e5f32583150d5b6631ae8e7
urtrial <msurtsukov@gmail.com>
2017-08-14 22:00:05 +0300

    pipeline start

--- libs/pipeline_utils.py
+++ libs/pipeline_utils.py
@@ -2,17 +2,19 @@ import numpy as np
 from libs.utils import filter_sequence
 
 
-def sample(enc_model, enc_session, dec_model, dec_session,
+def sample(enc_model, enc_session, enc_graph, dec_model, dec_session, dec_graph,
            dictionary, transformer, seed_phrase, n_items,
-           batch_size=64, max_iter=100):
-    state = enc_model.calculate_states(enc_session, transformer, phrases=[seed_phrase])[0]
-    state = np.vstack([state]*batch_size)
+           batch_size=64, max_iter=1000):
+    with enc_graph.as_default():
+        states = enc_model.calculate_states(enc_session, transformer, phrases=[seed_phrase])
+    batch_states = [np.vstack([state]*batch_size) for state in states]
     sampled = []
-    for i in range(max_iter):
-        sequences = dec_model.loop_sample(dec_session, transformer, state)
-        for seq in sequences:
-            sampled += filter_sequence(seq, dictionary=dictionary)
-        if len(sampled) >= n_items:
-            break
-    sampled = sampled[:n_items]
+    with dec_graph.as_default():
+        for i in range(max_iter):
+            sequences = dec_model.loop_sample(dec_session, transformer, batch_states)
+            for seq in sequences:
+                sampled += filter_sequence(seq, dictionary=dictionary)
+            if len(sampled) >= n_items:
+                break
+        sampled = sampled[:n_items]
     return sampled
